00d49a3ad8da30837e827b6a694ef124661d0e2e
Matheo813 <matheo.benic@gmail.com>
2022-09-30 15:58:37 +0200

    Modification Menu

--- src/components/Menus/AfricaMenus.js
+++ src/components/Menus/AfricaMenus.js
@@ -78,7 +78,7 @@ export default function AfricaMenus() {
 			image: mafeveg,
 			name: 'Mafé Végétarien',
 			price: '8 €',
-			description: 'Sauce arachide, carottes, courgettes, céleri',
+			description: 'Sauce arachide, carottes, courgettes, céleris',
 		},
 		{
 			image: yasaveg,
